f214076e8df0bcd2d49dc316766a6d18952ada7e
Marco Da Silva <markmarco16@hotmail.com>
2014-07-17 11:47:12 -0430

    first commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+plone policy for Plonecof2014 demo, for mastering in plone
+
+This product may contain traces of nuts.
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='ploneconf2014.policy',
+      version=version,
+      description="plone policy for Plonecof2014 demo",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='policy, demo, plone4',
+      author='Marco Da Silva',
+      author_email='markmarco16@hotmial.com',
+      url='https://github.com/markmarco16/Training-Plone',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ploneconf2014'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.contenttypes',
+          'collective.geo.bundle[dexterity]',
+          'wildcard.foldercontents',
+          'collective.plonetruegallery',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+        'test': ['plone.app.testing'],
+       },
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
