fa0f24ddd5068ac5da84e926638c92eeb4cf978f
Marco Da Silva <markmarco16@hotmail.com>
2014-07-18 15:58:14 -0430

    Integracion con Unittest

--- README.txt
+++
@@ -1,6 +0,0 @@
-Introduction
-============
-
-plone policy for Plonecof2014 demo, for mastering in plone
-
-This product may contain traces of nuts.
--- ploneconf2014/policy/config.py
+++ ploneconf2014/policy/config.py
@@ -5,3 +5,30 @@ Contains constants used by setuphandler.py
 """	
 
 PROJECTNAME = 'ploneconf2014.policy'
+
+DEPENDENCIES = [
+    'plone.api',
+	'plone.app.contenttypes',
+	'collective.geo.bundle[dexterity]',
+	'wildcard.foldercontents',
+	'collective.plonetruegallery',
+	'ploneconf2014.contenttypes',
+	'ploneconf2014.theme',
+    ]
+
+"""
+PRODUCT_DEPENDENCIES = [
+    'plone.api',
+	'plone.app.contenttypes',
+	'collective.geo.bundle[dexterity]',
+	'wildcard.foldercontents',
+	'collective.plonetruegallery',
+    ]
+
+PACKAGE_DEPENDENCIES = [
+	'ploneconf2014.contenttypes',
+	'ploneconf2014.theme',
+    ]
+
+DEPENDENCIES = PRODUCT_DEPENDENCIES + PACKAGE_DEPENDENCIES
+"""
